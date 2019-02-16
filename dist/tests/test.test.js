d18a401c1db31df4f6be57b4cccd5c3f2d8a4b97
Luca Campana <luca.campa.94@gmail.com>
2019-02-16 20:07:20 +0100

    Added dist folder

---
+++ dist/tests/test.test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const __1 = require("..");
+it('should be able to call dummy without crash and burn', () => {
+    __1.dummy();
+});
