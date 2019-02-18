511599064a6c16994600bdede120bd57c7b9e7e8
Luca Campana <luca.campa.94@gmail.com>
2019-02-18 21:04:43 +0100

    Removed find process test

--- tests/serverStart.test.ts
+++ tests/serverStart.test.ts
@@ -15,9 +15,3 @@ afterAll(async () => {
 it('should execute callback correctly', () => {
     expect(started).toBeTruthy();
 });
-
-it('should listen on correct port', async () => {
-    const find = require('find-process');
-    const list = await find('port', 2000);
-    expect(list[0].name).toBe('node.exe');
-});
