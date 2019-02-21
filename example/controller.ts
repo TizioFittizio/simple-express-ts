0de31c7a20d42c9f3b350420fd901ecbd8769ba7
Luca Campana <luca.campa.94@gmail.com>
2019-02-21 12:09:07 +0100

    Updated documentation and example

--- example/controller.ts
+++ example/controller.ts
@@ -1,18 +1,33 @@
-import { Get, Middleware, ExpressController } from '../lib';
+import { ExpressController, Get, Post, Middleware } from 'simple-express-ts';
 import { Request, Response, NextFunction } from 'express';
 
 export class Controller extends ExpressController {
 
     public controllerRoute: string = '/controller';
 
-    @Get('')
+    @Get('/test')
+    private async testRoute(req: Request, res: Response){
+        res.sendStatus(200);
+    }
+
+    @Post('/bodyParserTest')
+    private async testBodyParser(req: Request, res: Response){
+        const id = req.body.id;
+        res.send({id})
+    }
+
     @Middleware((req: Request, res: Response, next: NextFunction) => {
         req.params.value = 5;
         next();
     })
-    private async route(req: Request, res: Response){
+    @Middleware((req: Request, res: Response, next: NextFunction) => {
+        req.params.value = 10;
+        next();
+    })
+    @Get('/testMiddleware')
+    private async testMiddleware(req: Request, res: Response){
         const { value } = req.params;
-        res.send({ value }); // 5
+        res.send({ value });
     }
 
 }
