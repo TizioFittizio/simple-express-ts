c9b75a63386db38e934410df672f94bd13dbedfa
Luca Campana <luca.campa.94@gmail.com>
2019-05-12 23:47:42 +0200

    Fixed scoping in controller routes

--- tests/controller.test.ts
+++ tests/controller.test.ts
@@ -6,9 +6,29 @@ import * as request from 'supertest';
 @ExpressController('/test')
 class TestController {
 
+    private value: string;
+
+    constructor(){
+        this.value = '7';
+    }
+
     @Get('/test1')
-    private async get(req: Request, res: Response){
+    private async testGet(req: Request, res: Response){
         res.sendStatus(204);
+    }
+
+    @Get('/test2')
+    private async testValue(req: Request, res: Response){
+        res.send(this.value);
+    }
+
+    @Get('/test3')
+    private async testHelperMethod(req: Request, res: Response){
+        res.send(this.helperMethod('!'));
+    }
+
+    private helperMethod(value: string){
+        return value;
     }
 
 }
@@ -31,5 +51,23 @@ it('should be able to perform basic get', done => {
     request(server.app)
         .get('/test/test1')
         .expect(204)
+        .end(done);
+});
+
+it('should be able to obtain controller variables', done => {
+    request(server.app)
+        .get('/test/test2')
+        .expect(res => {
+            expect(res.text).toBe('7');
+        })
+        .end(done);
+});
+
+it('should be able to use controller methods', done => {
+    request(server.app)
+        .get('/test/test3')
+        .expect(res => {
+            expect(res.text).toBe('!');
+        })
         .end(done);
 });
